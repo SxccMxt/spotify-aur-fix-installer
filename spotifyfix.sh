464a29bccda05d31aa83d546bbf852e5aa55856d
SuccMut <82672367+SxccMxt@users.noreply.github.com>
2021-10-07 23:08:29 +0200

    added file spotifyfix.sh

---
+++ spotifyfix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# self explanatory, check package function
+function package_exists(){
+    pacman -Qi "$1" &> /dev/null
+    return $?
+}
+
+# checks if git package exists on system using pacman
+if package_exists git; then
+    printf 'git package found, continuing\n'
+else
+    sudo pacman -S git
+fi
+
+# checks if spotify is already installed, if not, replaces offending line with  
+# something that doesn't cause an error
+# and installs the spotify pkgbuild
+if package_exists spotify; then
+    printf 'spotify package is already installed. you should be able to update using an AUR helper, such as yay'
+else
+    git clone https://aur.archlinux.org/spotify.git 
+    search='chmod -R go-w "${pkgdir}"'
+    replace='find "${pkgdir}" ! -type l -exec chmod go-w {} +'
+    sed -i "s/$search/$replace/" ./spotify/PKGBUILD
+    makepkg -si
+fi
